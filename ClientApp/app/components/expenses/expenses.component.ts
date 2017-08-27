c014cefa0cd44ccd38ee1cf1eed45cffa0968d30
yarieldis <yarieldis@aleph.engineering>
2017-08-26 21:53:52 -0400

    Testing datepicker and select modules

--- ClientApp/app/components/expenses/expenses.component.ts
+++ ClientApp/app/components/expenses/expenses.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { Http } from '@angular/http';
+import {IOption} from 'ng-select';
+import {IMyDpOptions} from 'mydatepicker';
 
 @Component({
     selector: 'expenses',
@@ -8,6 +10,23 @@ import { Http } from '@angular/http';
 })
 export class ExpensesComponent {
     public data: Expenditure[];
+
+    public value:string = '';
+
+    types: Array<IOption> = [
+        {label: 'Belgium', value: 'BE'},
+        {label: 'Luxembourg', value: 'LU'},
+        {label: 'Netherlands', value: 'NL'}
+    ];
+    
+    private myDatePickerOptions: IMyDpOptions = {
+        // other options...
+        dateFormat: 'dd.mm.yyyy',
+    };
+
+    // Initialized to specific date (09.10.2018).
+    private model: Object = { date: { year: 2018, month: 10, day: 9 } };
+
 
     constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
         http.get(baseUrl + 'api/Expenses').subscribe(result => {
