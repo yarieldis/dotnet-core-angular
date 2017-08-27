3e818d5863418706b757fb3daa8311da1e7f9ed6
yarieldis <yarieldis@aleph.engineering>
2017-08-27 11:23:23 -0400

    Clean test code

--- ClientApp/app/components/expenses/expenses.component.ts
+++ ClientApp/app/components/expenses/expenses.component.ts
@@ -11,23 +11,6 @@ import {IMyDpOptions} from 'mydatepicker';
 export class ExpensesComponent {
     public data: Expenditure[];
 
-    public value:string = '';
-
-    types: Array<IOption> = [
-        {label: 'Belgium', value: 'BE'},
-        {label: 'Luxembourg', value: 'LU'},
-        {label: 'Netherlands', value: 'NL'}
-    ];
-    
-    private myDatePickerOptions: IMyDpOptions = {
-        // other options...
-        dateFormat: 'dd.mm.yyyy',
-    };
-
-    // Initialized to specific date (09.10.2018).
-    private model: Object = { date: { year: 2018, month: 10, day: 9 } };
-
-
     constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
         http.get(baseUrl + 'api/Expenses').subscribe(result => {
             this.data = result.json() as Expenditure[];
