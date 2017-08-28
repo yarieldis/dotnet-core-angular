acdcc0c4f7ec2cb536d3a7f572a180085a2a0563
yarieldis <yarieldis@aleph.engineering>
2017-08-27 20:49:31 -0400

    Use modal window component

--- ClientApp/app/components/expenses/expenses.component.ts
+++ ClientApp/app/components/expenses/expenses.component.ts
@@ -3,7 +3,10 @@ import { Http } from '@angular/http';
 import {IOption} from 'ng-select';
 import {IMyDpOptions} from 'mydatepicker';
 
+import { ModalService } from '../modal/modal.service';
+
 @Component({
+    moduleId: module.id.toString(),
     selector: 'expenses',
     templateUrl: './expenses.component.html',
     styleUrls: ['./expenses.component.css']
@@ -11,10 +14,18 @@ import {IMyDpOptions} from 'mydatepicker';
 export class ExpensesComponent {
     public data: Expenditure[];
 
-    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+    constructor(private modalService: ModalService, http: Http, @Inject('BASE_URL') baseUrl: string) {
         http.get(baseUrl + 'api/Expenses').subscribe(result => {
             this.data = result.json() as Expenditure[];
         }, error => console.error(error));
+    }
+
+    openModal(id: string){
+        this.modalService.open(id);
+    }
+
+    closeModal(id: string){
+        this.modalService.close(id);
     }
 }
 
