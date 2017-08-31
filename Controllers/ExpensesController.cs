a3b3a6f5916e4b1f5353ba2e59555685c6c4e418
yarieldis <yarieldis@aleph.engineering>
2017-08-31 13:55:31 -0400

    Refactor queries to Linq

--- Controllers/ExpensesController.cs
+++ Controllers/ExpensesController.cs
@@ -19,27 +19,7 @@ namespace test_angular.Controllers
         [HttpGet]
         public IEnumerable<Expenditure> Get()
         {
-            return _context.Expense.AsEnumerable().Select(expense => new Expenditure{
-                Description = expense.Description,
-                Amount = expense.Amount,
-                Date = expense.Date.ToString("yyyy-MM-dd"),
-                Type = expense.Type,
-                Id = expense.ID
-            });
-        }
-        [HttpGet("{id}")]
-        public Expenditure Get(int? id)
-        {
-            if (id == null)
-            {
-                return null;
-            }
-            var expense =  _context.Expense.SingleOrDefault(m => m.ID == id);
-            if (expense == null)
-            {
-                return null;
-            }
-            return new Expenditure {
+            return from expense in _context.Expense select new Expenditure {
                 Description = expense.Description,
                 Amount = expense.Amount,
                 Date = expense.Date.ToString("yyyy-MM-dd"),
@@ -47,6 +27,19 @@ namespace test_angular.Controllers
                 Id = expense.ID
             };
         }
+        [HttpGet("{id}")]
+        public Expenditure Get(int id)
+        {
+            var expense = (from e in _context.Expense where e.ID == id select e).SingleOrDefault();
+            return expense == null ? null : new Expenditure {
+                Description = expense.Description,
+                Amount = expense.Amount,
+                Date = expense.Date.ToString("yyyy-MM-dd"),
+                Type = expense.Type,
+                Id = expense.ID
+            };
+        }
+
         [HttpPost]
         public void Post([FromBody]Expenditure value)
         {
@@ -60,34 +53,28 @@ namespace test_angular.Controllers
             _context.SaveChanges();
         }
         [HttpPut("{id}")]
-        public void Put(int? id, [FromBody]Expenditure value)
+        public void Put(int id, [FromBody]Expenditure value)
         {
-            if (id != null)
+            var expense = (from e in _context.Expense where e.ID == id select e).SingleOrDefault();
+            if (expense != null)
             {
-                var expense =  _context.Expense.SingleOrDefault(m => m.ID == id);
-                if (expense != null)
-                {
-                    expense.Description = value.Description;
-                    expense.Amount = value.Amount;
-                    expense.Date = DateTime.Parse(value.Date);
-                    expense.Type = value.Type;
+                expense.Description = value.Description;
+                expense.Amount = value.Amount;
+                expense.Date = DateTime.Parse(value.Date);
+                expense.Type = value.Type;
 
-                    _context.Update(expense);
-                    _context.SaveChanges();
-                }
+                _context.Update(expense);
+                _context.SaveChanges();
             }
         }
         [HttpDelete("{id}")]
-        public void Delete(int? id)
+        public void Delete(int id)
         {
-            if (id != null)
+            var expense = (from e in _context.Expense where e.ID == id select e).SingleOrDefault();
+            if (expense != null)
             {
-                var expense =  _context.Expense.SingleOrDefault(m => m.ID == id);
-                if (expense != null)
-                {
-                    _context.Remove(expense);
-                    _context.SaveChanges();
-                }
+                _context.Remove(expense);
+                _context.SaveChanges();
             }
         }
         public class Expenditure
